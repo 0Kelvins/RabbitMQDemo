f5a1f75349d9fa58fde76f731ddff90a316e0ed2
0Kelvins <554961401@qq.com>
2017-11-15 20:20:04 +0800

    upload

---
+++ Recieve/Program.cs
@@ -0,0 +1,105 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Recieve
+{
+    class Program
+    {
+        static void recieveHello()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void recieveInput()
+        {
+            string threadName = Thread.CurrentThread.Name;  // 下面内部使用了新的线程接受消息
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "input",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" {0} Received {1}", threadName, message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    Console.WriteLine(" {0} Done", threadName);
+                };
+                channel.BasicConsume(queue: "input", autoAck: false, consumer: consumer);
+                
+                Console.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Type: ");
+            string type = Console.ReadLine();
+
+            switch (type)
+            {
+                case "1":
+                    recieveHello();
+                    break;
+                case "2":
+                    Thread thread1 = new Thread(new ThreadStart(recieveInput));
+                    thread1.Name = "线程1";
+                    Thread thread2 = new Thread(new ThreadStart(recieveInput));
+                    thread2.Name = "线程2";
+
+                    thread1.Start();
+                    thread2.Start();
+
+                    Console.WriteLine(" Press [enter] twice to exit.");
+
+                    thread1.Join();
+                    thread2.Join();
+                    break;
+                default:
+                    Console.WriteLine("Not Exist!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,85 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void sendHello(ConnectionFactory factory)
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+
+        static void sendInput(ConnectionFactory factory)
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "input",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                while (true)
+                {
+                    var message = Console.ReadLine();
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "input",
+                                         basicProperties: properties,
+                                         body: body);
+                   
+                    if ("q".Equals(message))
+                        break;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            Console.Write("Type: ");
+            string type = Console.ReadLine();
+            switch (type)
+            {
+                case "1":
+                    sendHello(factory);
+                    break;
+                case "2":
+                    sendInput(factory);
+                    break;
+                default:
+                    Console.WriteLine("Not Exist!");
+                    break;
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
