e3488a005343f5b04236df98e3a5a024cf936c86
0Kelvins <554961401@qq.com>
2017-11-17 17:31:25 +0800

    update README

--- README.md
+++ README.md
@@ -10,3 +10,7 @@
 channel的方法参数解释：
 [RabbitMQ channel参数详解 —— zhangxuhui 博客园](https://www.cnblogs.com/piaolingzxh/p/5448927.html)
 
+记录：
+* 主要代码都是官网的C#教程内的例子
+* 主要是练习并记录一下备用
+* 先选择Recieve后选择Send比较好（否则没有持久化，或者没有多次输入的会发完收不到）
--- Send/Program.cs
+++ Send/Program.cs
@@ -11,7 +11,7 @@ namespace Send
         /// </summary>
         public static void Main(string[] args)
         {
-            Console.WriteLine(" 生产者类型对应key：");
+            Console.WriteLine(" 生产者类型对应key（先选择Recieve后Send）：");
             Console.WriteLine(" 1： SimpleProducer 简单的，给一个队列发送消息，一个消费者接收消息");
             Console.WriteLine(" 2： WorkQueueProducer 工作队列，发送给指定队列信息，队列上的所有消费者争夺消息");
             Console.WriteLine(" 3： PublishProducer Fanout，所有绑定在对应交换器上的队列都会收到每一条消息");
